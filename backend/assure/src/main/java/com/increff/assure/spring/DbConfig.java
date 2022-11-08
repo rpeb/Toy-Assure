8e1430bdcd49d6dcff5ed1955b7c94c15942584e
rpeb <prakashdubey0745@gmail.com>
2022-11-08 22:24:52 +0530

    added a naming strategy to hibernate

--- backend/assure/src/main/java/com/increff/assure/spring/DbConfig.java
+++ backend/assure/src/main/java/com/increff/assure/spring/DbConfig.java
@@ -17,61 +17,64 @@ import java.util.Properties;
 @Configuration
 public class DbConfig {
 
-	public static final String PACKAGE_POJO = "com.increff.assure.pojo";
-	
-	@Value("${jdbc.driverClassName}")
-	private String jdbcDriver;
-	@Value("${jdbc.url}")
-	private String jdbcUrl;
-	@Value("${jdbc.username}")
-	private String jdbcUsername;
-	@Value("${jdbc.password}")
-	private String jdbcPassword;
-	@Value("${hibernate.dialect}")
-	private String hibernateDialect;
-	@Value("${hibernate.show_sql}")
-	private String hibernateShowSql;
-	@Value("${hibernate.hbm2ddl.auto}")
-	private String hibernateHbm2ddl;
-	
+    public static final String PACKAGE_POJO = "com.increff.assure.pojo";
 
-	@Bean(name = "dataSource")
-	public DataSource getDataSource() {
-		BasicDataSource bean = new BasicDataSource();
-		bean.setDriverClassName(jdbcDriver);
-		bean.setUrl(jdbcUrl);
-		bean.setUsername(jdbcUsername);
-		bean.setPassword(jdbcPassword);
-		bean.setInitialSize(2);
-		bean.setDefaultAutoCommit(false);
-		bean.setMinIdle(2);
-		bean.setValidationQuery("Select 1");
-		bean.setTestWhileIdle(true);
-		bean.setTimeBetweenEvictionRunsMillis(10 * 60 * 100);
-		return bean;
-	}
+    @Value("${jdbc.driverClassName}")
+    private String jdbcDriver;
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${jdbc.username}")
+    private String jdbcUsername;
+    @Value("${jdbc.password}")
+    private String jdbcPassword;
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
+    @Value("${hibernate.show_sql}")
+    private String hibernateShowSql;
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hibernateHbm2ddl;
+    @Value("${hibernate.physical_naming_strategy}")
+    private String hibernatePhysicalNamingStrategy;
 
-	@Bean(name = "entityManagerFactory")
-	@Autowired
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-		LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-		bean.setDataSource(dataSource);
-		bean.setPackagesToScan(PACKAGE_POJO);
-		HibernateJpaVendorAdapter jpaAdapter = new HibernateJpaVendorAdapter();
-		bean.setJpaVendorAdapter(jpaAdapter);
-		Properties jpaProperties = new Properties();
-		jpaProperties.put("hibernate.dialect", hibernateDialect);
-		jpaProperties.put("hibernate.show_sql", hibernateShowSql);
-		jpaProperties.put("hibernate.hbm2ddl.auto", hibernateHbm2ddl);
-		bean.setJpaProperties(jpaProperties);
-		return bean;
-	}
-	
-	@Bean(name = "transactionManager")
-	@Autowired
-	public JpaTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean emf) {
-		JpaTransactionManager bean = new JpaTransactionManager();
-		bean.setEntityManagerFactory(emf.getObject());
-		return bean;
-	}
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        BasicDataSource bean = new BasicDataSource();
+        bean.setDriverClassName(jdbcDriver);
+        bean.setUrl(jdbcUrl);
+        bean.setUsername(jdbcUsername);
+        bean.setPassword(jdbcPassword);
+        bean.setInitialSize(2);
+        bean.setDefaultAutoCommit(false);
+        bean.setMinIdle(2);
+        bean.setValidationQuery("Select 1");
+        bean.setTestWhileIdle(true);
+        bean.setTimeBetweenEvictionRunsMillis(10 * 60 * 100);
+        return bean;
+    }
+
+    @Bean(name = "entityManagerFactory")
+    @Autowired
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setPackagesToScan(PACKAGE_POJO);
+        HibernateJpaVendorAdapter jpaAdapter = new HibernateJpaVendorAdapter();
+        bean.setJpaVendorAdapter(jpaAdapter);
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", hibernateDialect);
+        jpaProperties.put("hibernate.show_sql", hibernateShowSql);
+        jpaProperties.put("hibernate.hbm2ddl.auto", hibernateHbm2ddl);
+        jpaProperties.put("hibernate.physical_naming_strategy", hibernatePhysicalNamingStrategy);
+        bean.setJpaProperties(jpaProperties);
+        return bean;
+    }
+
+    @Bean(name = "transactionManager")
+    @Autowired
+    public JpaTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean emf) {
+        JpaTransactionManager bean = new JpaTransactionManager();
+        bean.setEntityManagerFactory(emf.getObject());
+        return bean;
+    }
 }
