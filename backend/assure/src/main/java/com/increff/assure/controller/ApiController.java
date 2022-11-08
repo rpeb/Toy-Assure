c187dbf35a8ac0415d76de0bc43bc1e9ca8aa7d6
rpeb <prakashdubey0745@gmail.com>
2022-11-08 22:25:28 +0530

    changed the uri

--- backend/assure/src/main/java/com/increff/assure/controller/ApiController.java
+++ backend/assure/src/main/java/com/increff/assure/controller/ApiController.java
@@ -2,18 +2,16 @@ package com.increff.assure.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api
 @RestController
-@RequestMapping("/api")
 public class ApiController {
     @RequestMapping(value = "/health", method = RequestMethod.GET)
     @ApiOperation(value = "health check endpoint")
     public String sayHello() {
-        return "hello from api controller";
+        return "hello from assure module";
     }
 }
