bd3d2eda8e6274a0e56e244fc2439622560f1e02
rpeb <prakashdubey0745@gmail.com>
2022-11-08 22:24:01 +0530

    constants for table names

---
+++ backend/assure/src/main/java/com/increff/assure/model/TableConstants.java
@@ -0,0 +1,13 @@
+package com.increff.assure.model;
+
+public class TableConstants {
+    public static final int SEQ_INITIAL_VALUE = 10000000;
+
+    public static final String SEQ_USER = "seq_user";
+    public static final String SEQ_PRODUCT = "seq_product";
+    public static final String SEQ_BIN = "seq_bin";
+    public static final String SEQ_BINSKU = "seq_binsku";
+    public static final String SEQ_INVENTORY = "seq_inventory";
+    public static final String SEQ_CHANNEL = "seq_channel";
+    public static final String SEQ_CHANNEL_LISTING = "seq_channel_listing";
+}
